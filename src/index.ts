9230cc3b172f9774ba5cee3fb58bf24ba4e08135
Rodrigo Alves <rodrigoalves009@gmail.com>
2022-08-30 20:03:32 -0300

    added manual cors

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,21 @@ const database = new Neo4JConnection();
 
 database.connect();
 
-app.use(cors({ origin: '*' }));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested, Content-Type, Accept Authorization"
+    )
+    if (req.method === "OPTIONS") {
+      res.header(
+        "Access-Control-Allow-Methods",
+        "POST, PUT, PATCH, GET, DELETE"
+      )
+      return res.status(200).json({})
+    }
+    next()
+  })
 app.use(express.json());
 app.use('/api/users', routes(database));
 
