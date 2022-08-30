b690d4e0b0f27ee24556f6d8ec5b79c727764fed
Rodrigo Alves <rodrigoalves009@gmail.com>
2022-08-30 19:51:09 -0300

    added origin *

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const database = new Neo4JConnection();
 
 database.connect();
 
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 app.use('/api/users', routes(database));
 
